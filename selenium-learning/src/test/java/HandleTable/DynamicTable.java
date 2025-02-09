61d7d39e5d382ba2052f2d19c3e07f506f67d413
Ilham Imani Nur Alam <ilhamimaninuralam@gmail.com>
2025-02-10 00:08:26 +0700

    add handling pagination and read data from table

--- selenium-learning/src/test/java/HandleTable/DynamicTable.java
+++ selenium-learning/src/test/java/HandleTable/DynamicTable.java
@@ -4,12 +4,13 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DynamicTable {
-public static void main(String[] args) {
+public static void main(String[] args) throws InterruptedException {
 	WebDriver driver = new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.get("https://www.ecomdeveloper.com/demo/admin/index.php");
@@ -32,6 +33,23 @@ public static void main(String[] args) {
 	}
 	driver.findElement(By.xpath("//a[@class='parent collapsed'][normalize-space()='Customers']")).click();
 	driver.findElement(By.xpath("//ul[@id='collapse9']//a[contains(text(),'Customers')]")).click();
+	String text = driver.findElement(By.xpath("//div[contains(text(),('Pages'))]")).getText();
+	System.out.println(text);
+	int total_pages=Integer.parseInt(text.substring(text.indexOf("(")+1,text.indexOf("Pages")-1));
 	
+	for(int p=1;p<=3;p++) {
+		if(p>1) {
+			WebElement active_page = driver.findElement(By.xpath("//ul[@class='pagination']//*[text()="+p+"]"));
+			active_page.click();
+			Thread.sleep(3000);
+		}
+	
+	int noRows = driver.findElements(By.xpath("//table[@class='table table-bordered table-hover']//tbody/tr")).size();
+	for(int r =1 ;r<=noRows;r++) {
+		String username = driver.findElement(By.xpath("//table[@class='table table-bordered table-hover']//tbody/tr["+r+"]//td[2]")).getText();
+		String email = driver.findElement(By.xpath("//table[@class='table table-bordered table-hover']//tbody/tr["+r+"]//td[3]")).getText();
+		System.out.println(username+"\t"+email);
+	}
+}
 }
 }
